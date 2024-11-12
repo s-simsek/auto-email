4275e9c6a39ca02443a9ac164e018fa2f78562e8
Safak Simsek <safaksimsek05@gmail.com>
2024-11-11 20:28:59 -0800

    code cleaning

--- auto_email.py
+++ auto_email.py
@@ -4,18 +4,7 @@ from email.message import EmailMessage
 
 from dotenv import load_dotenv
 
-from get_data import get_data
-
 load_dotenv()
-
-mens_winner, womens_winner = get_data()
-
-subject = "NHD Contest Winner Announcement"
-body = "Dear Adrian \n\nThe winner of the 2024 Nathan's Hot Dog Eating Contest is: \n\nMEN'S WINNER: " + \
-        mens_winner + "\nWOMEN'S WINNER: " + womens_winner + "\n\nBest regards,\nSafak Simsek"
-to_email = "safaksimsek5@berkeley.edu"
-from_email = "safaksimsek05@gmail.com"
-
 
 def send_email(body, subject, to_email, from_email):
     password = os.getenv('APP_PASSWORD')

dd41c01af3cc515cc4db0bae43375519a637cf94
Safak Simsek <safaksimsek05@gmail.com>
2024-11-12 09:48:05 -0800

    separating scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,31 +1,32 @@
 import datetime
-import time
+import os
+import threading
 
-import schedule
+from apscheduler.schedulers.blocking import BlockingScheduler
+from dotenv import load_dotenv
 
 from auto_email import send_email
 from get_data import get_data
 
+load_dotenv()
+
+# prepare email content
 mens_winner, womens_winner = get_data()
 subject = "NHD Contest Winner Announcement"
 body = "Dear Adrian, \n\nThe winner of the 2024 Nathan's Hot Dog Eating Contest is: \n\nMEN'S WINNER: " + \
-    mens_winner + "\nWOMEN'S WINNER: " + womens_winner + "\n\nBest regards,\nSafak Simsek"
+        mens_winner + "\nWOMEN'S WINNER: " + womens_winner + "\n\nBest regards,\nSafak Simsek"
 to_email = "safaksimsek5@berkeley.edu"
 from_email = "safaksimsek05@gmail.com"
 
 def scheduled_email_job():
-    # Send email    
     status = send_email(body, subject, to_email, from_email)
     if status == "Success":
-        print({"time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-               "subject": subject,
-               "body": body,
-               "to_email": to_email})
+        print(f"Email sent successfully at {datetime.datetime.now()}")
     else:
         print(f"Failed to send email: {status}")
-        
+
 if __name__ == '__main__':
-    schedule.every(2).minutes.do(scheduled_email_job)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)   
+    scheduler = BlockingScheduler()
+    scheduler.add_job(scheduled_email_job, 'interval', minutes=1)
+    print("Scheduler started...")
+    scheduler.start()

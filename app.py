e9dfce2e373d3a451bbd8ad5dbf1b4b43174cdef
Safak Simsek <safaksimsek05@gmail.com>
2024-11-12 10:02:14 -0800

    add database and read sent email functionality

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+# app.py
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+from models import EmailRecord, Session  # Import from your models module
+
 app = Flask(__name__)
+load_dotenv()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    session = Session()
+    emails = session.query(EmailRecord).order_by(EmailRecord.time_sent.desc()).all()
+    session.close()
+    return render_template('index.html', emails=emails)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)  # use_reloader=False is important to not interfere with APScheduler
+    app.run(debug=True, use_reloader=False)
--- scheduler.py
+++ scheduler.py
@@ -1,27 +1,43 @@
+# scheduler.py
 import datetime
 import os
-import threading
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from dotenv import load_dotenv
 
 from auto_email import send_email
 from get_data import get_data
+from models import EmailRecord, Session  # Import from your models module
 
 load_dotenv()
 
-# prepare email content
+# Prepare email content
 mens_winner, womens_winner = get_data()
 subject = "NHD Contest Winner Announcement"
-body = "Dear Adrian, \n\nThe winner of the 2024 Nathan's Hot Dog Eating Contest is: \n\nMEN'S WINNER: " + \
-        mens_winner + "\nWOMEN'S WINNER: " + womens_winner + "\n\nBest regards,\nSafak Simsek"
-to_email = "safaksimsek5@berkeley.edu"
+body = (
+    "Dear Adrian, \n\nThe winner of the 2024 Nathan's Hot Dog Eating Contest is: \n\nMEN'S WINNER: "
+    + mens_winner
+    + "\nWOMEN'S WINNER: "
+    + womens_winner
+    + "\n\nBest regards,\nSafak Simsek"
+)
+to_email = "defne@dayioglu.com"
 from_email = "safaksimsek05@gmail.com"
 
 def scheduled_email_job():
     status = send_email(body, subject, to_email, from_email)
     if status == "Success":
         print(f"Email sent successfully at {datetime.datetime.now()}")
+        # Save email record to the database
+        session = Session()
+        email_record = EmailRecord(
+            subject=subject,
+            body=body,
+            to_email=to_email,
+        )
+        session.add(email_record)
+        session.commit()
+        session.close()
     else:
         print(f"Failed to send email: {status}")
 
